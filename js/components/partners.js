a02096e304f08a87ba6e376e10fb2fa764b47bae
Rodrigo Chacon <rodrixchacon@gmail.com>
2026-02-26 18:58:37 -0700

    changes 4

--- js/components/partners.js
+++ js/components/partners.js
@@ -1,12 +1,14 @@
+const _partnersScript = document.currentScript;
 async function loadPartners() {
+  const base = _partnersScript.getAttribute('src').replace('js/components/partners.js', '');
   // Load CSS
   const link = document.createElement('link');
   link.rel = 'stylesheet';
-  link.href = '/css/components/partners.css';
+  link.href = base + 'css/components/partners.css';
   document.head.appendChild(link);
 
   // Load HTML
-  const res = await fetch('/components/partners.html');
+  const res = await fetch(base + 'components/partners.html');
   const html = await res.text();
   document.getElementById('partners').innerHTML = html;
 }
